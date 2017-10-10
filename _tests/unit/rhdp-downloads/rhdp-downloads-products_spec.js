db707a16eaabe0134d65288f4febb9d69b3a2767
dcoughlin <daniel.coughlin86@gmail.com>
2017-10-10 09:23:50 -0700

    DEVELOPER-3587 - Added more updates to product list logic

--- _tests/unit/rhdp-downloads/rhdp-downloads-products_spec.js
+++ _tests/unit/rhdp-downloads/rhdp-downloads-products_spec.js
@@ -23,26 +23,46 @@ describe('Download Products', function() {
             expect(wc.getAttribute('category')).toEqual(category);
         });
         it('should update data attribute', function() {
-            var data = 'http://www.testproduct.com/';
+            var data =
+                [
+                    {
+                        "name": "Data Virtualization",
+                        "productCode": "datavirt",
+                        "featuredArtifact": {
+                            "url": "https://developers.stage.redhat.com/download-manager/content/origin/files/sha256/b4/b466affbcc1740bf2c7c73b60bb6ffa7e1ec844fc08447224ab15aa3bcee3949/jboss-dv-6.3.0-1-installer.jar",
+                            "description": "Installer",
+                            "label": "Installer",
+                            "fileSize": 1001265730,
+                            "tcModel": "JBoss/Red Hat Developer Subscription",
+                            "sha256": "b466affbcc1740bf2c7c73b60bb6ffa7e1ec844fc08447224ab15aa3bcee3949",
+                            "md5": "f88f9106745f913c9e2e158cd28eebf7",
+                            "versionName": "6.3.0",
+                            "releaseDate": 1470801600000,
+                            "type": "file"
+                        },
+                        "productVersions": []
+                    }
+                ]
+            ;
             wc.data = data;
-            expect(wc.getAttribute('data')).toEqual(data);
+            expect(wc.data).toEqual(data);
         });
-        it('should update products attribute', function() {
-            var products = {
-                "products": [{
-                    "productName": "Red Hat JBoss Data Virtualization",
-                    "groupHeading": "INTEGRATION AND AUTOMATION",
-                    "productCode": "datavirt",
-                    "featured": false,
-                    "downloadLink": "https://developers.stage.redhat.com/download-manager/content/origin/files/sha256/b4/b466affbcc1740bf2c7c73b60bb6ffa7e1ec844fc08447224ab15aa3bcee3949/jboss-dv-6.3.0-1-installer.jar",
-                    "description": "A tool that brings operational and analytical insight from data dispersed in various business units, apps, and technologies.",
-                    "version": "6.3.0",
-                    "learnMoreLink": "https://developers.redhat.com/products/datavirt/overview/"
-                }]
-            };
-            wc.products = products;
-            expect(wc.products).toEqual(products);
-        });
+        // it('should update products attribute', function() {
+        //     var products = {
+        //         "products": [{
+        //             "productName": "Red Hat JBoss Data Virtualization",
+        //             "groupHeading": "INTEGRATION AND AUTOMATION",
+        //             "productCode": "datavirt",
+        //             "featured": false,
+        //             "downloadLink": "https://developers.stage.redhat.com/download-manager/content/origin/files/sha256/b4/b466affbcc1740bf2c7c73b60bb6ffa7e1ec844fc08447224ab15aa3bcee3949/jboss-dv-6.3.0-1-installer.jar",
+        //             "description": "A tool that brings operational and analytical insight from data dispersed in various business units, apps, and technologies.",
+        //             "version": "6.3.0",
+        //             "learnMoreLink": "https://developers.redhat.com/products/datavirt/overview/"
+        //         }]
+        //     };
+        //     wc.products = products;
+        //     expect(wc.products).toEqual(products);
+        // });
 
     });
 
@@ -60,13 +80,9 @@ describe('Download Products', function() {
 
         it('should be contain a valid download link and version name', function() {
             wc.data = mockData;
-            expect(wc.products).toEqual({"products":[{"productName":"Red Hat JBoss Data Virtualization","groupHeading":"INTEGRATION AND AUTOMATION","productCode":"datavirt","featured":false,"downloadLink":"https://developers.stage.redhat.com/download-manager/content/origin/files/sha256/b4/b466affbcc1740bf2c7c73b60bb6ffa7e1ec844fc08447224ab15aa3bcee3949/jboss-dv-6.3.0-1-installer.jar","description":"A tool that brings operational and analytical insight from data dispersed in various business units, apps, and technologies.","version":"6.3.0","learnMoreLink":"https://developers.redhat.com/products/datavirt/overview/"}]});
+            expect(wc.products).toEqual({"products":[{"productName":"Red Hat JBoss Data Virtualization","groupHeading":"INTEGRATION AND AUTOMATION","productCode":"datavirt","featured":false,"dataFallbackUrl":"https://access.redhat.com/jbossnetwork/restricted/listSoftware.html?product=data.services.platform&downloadType=distributions","downloadLink":"","description":"A tool that brings operational and analytical insight from data dispersed in various business units, apps, and technologies.","version":"","learnMoreLink":"https://developers.redhat.com/products/datavirt/overview/"}]});
         });
 
-        it('should return a list by category when requested', function() {
-            var categoryList = wc.getProductsWithHeading(heading)
-            expect(categoryList).toEqual(categoryList);
-        });
 
     })
 
